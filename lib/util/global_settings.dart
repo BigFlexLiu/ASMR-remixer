5bbb24c212271006bf4ee0ff8af0305405f00508
Felix Liu <Felixliu135@gmail.com>
2022-12-19 11:42:10 -0500

    Prevent using balance on audio as it is not supported on mobile

---
+++ lib/util/global_settings.dart
@@ -0,0 +1,6 @@
+// This file contains all settings that affects the entire project
+
+// Currently disabled due to AudioPlayer library having no support for it
+// on mobiles
+// See https://github.com/bluefireteam/audioplayers/blob/main/feature_parity_table.md
+const IS_BALANCE_ENABLED = true;
